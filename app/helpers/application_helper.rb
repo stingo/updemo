c1b1d8ba1fe1e231de0354a6cd15fca733e0acf1
Charles Wiredu <afrowebs@gmail.com>
2020-10-20 23:56:07 +0100

    layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def avataryze(name)
+    as_array = name.split(' ')
+    if as_array.length >= 2 then (as_array[0][0] + as_array[1][0]).upcase else name[0..1].upcase end
+  end
 end
+
